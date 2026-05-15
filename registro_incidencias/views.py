1dffa14140ba1e7964a570905d63408385d1eaa9
ak13a <ak13a@tutamail.com>
2026-05-15 09:17:56 -0500

    feat: definir vistas de apis

--- registro_incidencias/views.py
+++ registro_incidencias/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Ciudadano, Incidencia
+from .serializers import CiudadanoSerializer, IncidenciaSerializer
 
 # Create your views here.
+
+class CiudadanoViewSet(viewsets.ModelViewSet):
+    queryset = Ciudadano.objects.all()
+    serializer_class = CiudadanoSerializer
+
+class IncidenciaViewSet(viewsets.ModelViewSet):
+    queryset = Incidencia.objects.all()
+    serializer_class = IncidenciaSerializer
